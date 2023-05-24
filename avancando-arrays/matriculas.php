67e72124e5e9e27ea32e6e8f3fba06b4e1faeda6
Daniel Carvalho <danielcarvalhodev@gmail.com>
2023-05-24 19:59:19 -0300

    Aula final - Arrays avançados

--- avancando-arrays/matriculas.php
+++ avancando-arrays/matriculas.php
@@ -19,4 +19,13 @@ $novosAlunos = [
 //$alunos2022 = $alunos2021 + $novosAlunos;
 $alunos2022 = [...$alunos2021, 'Carlos Vinicius' ,...$novosAlunos];
 
+array_push($alunos2022, 'Alice', 'Charlie', 'Bob');
+$alunos2022[] = 'Luiz';
+
+array_unshift($alunos2022, 'Stefanie', 'Rafa');
+
+echo array_shift($alunos2022) . PHP_EOL;
+
+echo array_pop($alunos2022) . PHP_EOL;
+
 var_dump($alunos2022);
